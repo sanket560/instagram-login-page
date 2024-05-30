20579682f609efb905716353b556a223a77f2d9a
sanket560 <sanketmmane@gmail.com>
2024-05-30 03:36:09 -0400

    made responsive

--- app/page.js
+++ app/page.js
@@ -12,6 +12,8 @@ export default function Home() {
     password: "",
   });
 
+  const [isSubmitting, setIsSubmitting] = useState(false);
+
   const handleInput = (e) => {
     const { name, value } = e.target;
     setUser((prevState) => ({
@@ -22,6 +24,7 @@ export default function Home() {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+    setIsSubmitting(true);
     try {
       const response = await fetch("/api/login", {
         method: "POST",
@@ -33,32 +36,31 @@ export default function Home() {
 
       const data = await response.json();
       if (data.success) {
-         setUser({
-           username: "",
-           password: "",
-         });
+        setUser({
+          username: "",
+          password: "",
+        });
       }
     } catch (error) {
       console.log("Error:", error);
     } 
   };
 
-
   return (
-    <main className="flex flex-col items-center justify-between py-14">
-      <div className="flex gap-3">
-        <div className="-mt-5">
+    <main className="flex -ml-8 flex-col items-center justify-between py-14">
+      <div className="flex gap-3 -mt-10 md:mt-0">
+        <div className="md:-mt-5 -mt-10">
           <Image
             src={img}
-            className="w-[400px] h-[600px]"
+            className="w-[400px] h-[600px] hidden md:block"
             width={300}
             height={400}
           />
         </div>
         <div className="flex-1 flex items-center justify-center">
           <div className="h-screen flex flex-col items-center">
-            <div className="bg-white border border-gray-300 w-80 py-8 flex items-center flex-col mb-3">
-              <Image className="w-40" src={img1} width={200} height={300} />
+            <div className="bg-white md:border md:border-gray-300 w-80 py-8 flex items-center flex-col mb-3">
+              <Image className="w-40 " src={img1} width={200} height={300} />
               <form onSubmit={handleSubmit} className="mt-8 w-64 flex flex-col">
                 <input
                   className="text-xs w-full mb-2 rounded border bg-gray-100 border-gray-300 px-2 py-2 focus:outline-none focus:border-gray-400 active:outline-none"
@@ -78,7 +80,11 @@ export default function Home() {
                   value={user.password}
                   onChange={handleInput}
                 />
-                <button type="submit" className=" text-sm text-center bg-[#4cb5f9] text-white py-2 rounded-lg font-medium">
+                <button
+                  type="submit"
+                  className="text-sm text-center bg-[#4cb5f9] text-white py-2 rounded-lg font-medium"
+                  disabled={isSubmitting}
+                >
                   Log In
                 </button>
               </form>
@@ -99,7 +105,7 @@ export default function Home() {
                 Forgot password?
               </a>
             </div>
-            <div className="bg-white border border-gray-300 text-center w-80 py-4">
+            <div className="bg-white md:border md:border-gray-300 text-center w-80 py-4">
               <span className="text-sm">Don&apos;t have an account?</span>
               <a className="text-blue-500 text-sm mx-1 font-semibold">
                 Sign up
@@ -115,8 +121,8 @@ export default function Home() {
           </div>
         </div>
       </div>
-      <div className="-mt-36 p-4 text-sm text-gray-600">
-        <div className="flex justify-center items-center">
+      <div className="-mt-36 p-10 md:p-4 text-sm text-gray-600">
+        <div className="flex flex-wrap justify-center items-center">
           <p className="m-2">Meta</p>
           <p className="m-2">About</p>
           <p className="m-2">Blog</p>
